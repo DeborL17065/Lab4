3ebf6dc150b63d60019a38ead98fca52f8eb0e7d
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-17 16:57:40 -0600

    Update SPI.c

--- Lab4_MASTER.X/SPI.c
+++ Lab4_MASTER.X/SPI.c
@@ -5,12 +5,21 @@
  * Created on February 11, 2020, 11:28 PM
  */
 
+//MASTER
+
 
 #include <xc.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 #include "SPI.h"
 
 
-void CONF_SPI(void){
+void CONF_SPI(void) {
+  //  ADCON1 = 0XFF;
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
     //////////////////////////////////////////////////////
     TRISCbits.TRISC5 =0; //SDO SALIDA DE DATOS
     TRISCbits.TRISC3 =0; //SCK ENTRADA DEL RELOJ
@@ -20,11 +29,28 @@ void CONF_SPI(void){
     SSPSTATbits.SMP = 0; //
     SSPSTATbits.CKE = 0;
     //-------CONFIGURACION SSPCON---------------------
-    SSPCON = 0b00000000;
+    SSPCON = 0b0010;
     SSPCONbits.CKP = 1; //ESTADO IDLE DEL RELOJ ES NIVEL ALTO
     SSPCONbits.SSPEN = 1; //ACTIVAR PUERTO SERIAL
     //SSPCONbits.SSPOV = 0; //LIMPIAR BANDERA OVERFLOW
     //////////////////////////////////////////////////////////
-
-
 }
+void SPIWRITE (char dato){
+    PORTAbits.RA5 =0;
+    SSPBUF = dato;
+    PORTAbits.RA5 =1;
+    return;
+    //while(!SSPSTATbits.BF); //ESPERAR A QUE CAMBIE LA BANDERA
+   // SSPCONbits.SSPOV = 0; //LIMPIAR OVERFLOW BANDERA
+}
+
+void SPIREAD (void){
+    PORTAbits.RA5 =0;
+    if (SSPSTATbits.BF == 1){
+        RANDOM = SSPBUF;
+        SSPSTATbits.BF =0;  
+        SSPCONbits.SSPOV = 0;
+    }
+    return;
+}
+
