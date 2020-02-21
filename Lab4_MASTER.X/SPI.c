8c6de7b00b0d575307fe7c3637d425e770c6d9c0
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-02-21 12:54:35 -0600

    Update SPI.c
    FUNCIONANDO AL 100%

--- Lab4_MASTER.X/SPI.c
+++ Lab4_MASTER.X/SPI.c
@@ -20,38 +20,34 @@ void CONF_SPI(void) {
   //  ADCON1 = 0XFF;
     INTCONbits.GIE = 1;
     INTCONbits.PEIE = 1;
+    PIE1bits.SSPIE = 1;
     //////////////////////////////////////////////////////
     TRISCbits.TRISC5 =0; //SDO SALIDA DE DATOS
-    TRISCbits.TRISC3 =0; //SCK ENTRADA DEL RELOJ
+    TRISCbits.TRISC3 =0; //SCK SALIDA DEL RELOJ
     TRISCbits.TRISC4 =1; //SDI ENTRADA DE DATOS
-    TRISAbits.TRISA5 =0; //SS SALIDA PARA EL MAESTRO
+    TRISAbits.TRISA4 =0; //SS SALIDA PARA EL MAESTRO
     //-------CONFIGURACION SSPSTAT---------------------
     SSPSTATbits.SMP = 0; //
     SSPSTATbits.CKE = 0;
+    
     //-------CONFIGURACION SSPCON---------------------
-    SSPCONbits.SSPM = 0b0010;
     SSPCONbits.CKP = 1; //ESTADO IDLE DEL RELOJ ES NIVEL ALTO
+    SSPCONbits.SSPM = 0b0000;
     SSPCONbits.SSPEN = 1; //ACTIVAR PUERTO SERIAL
     //SSPCONbits.SSPOV = 0; //LIMPIAR BANDERA OVERFLOW
     //////////////////////////////////////////////////////////
 }
 void SPIWRITE (char dato){
-    PORTAbits.RA5 =0;
     SSPBUF = dato;
-    PORTAbits.RA5 =1;
-    return;
-    //while(!SSPSTATbits.BF); //ESPERAR A QUE CAMBIE LA BANDERA
-   // SSPCONbits.SSPOV = 0; //LIMPIAR OVERFLOW BANDERA
+    
 }
 
-void SPIREAD (void){
-    PORTAbits.RA5 =0;
-    if (SSPSTATbits.BF == 1){
-        RANDOM = SSPBUF;
-        SSPSTATbits.BF =0;  
-        SSPCONbits.SSPOV = 0;
-    }
+void SPIREAD(void){
     
+    while(!SSPSTATbits.BF ); //ESPERAMOS A QUE EL VALOR LLEGUE POR COMPLETO
+    RANDOM = SSPBUF; 
     return;
+
+      
 }
 
